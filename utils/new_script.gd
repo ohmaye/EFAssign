08155550136d49498c3046e3275b85c372d93fb7
Enio Ohmaye <enio.ohmaye@gmail.com>
2024-09-18 11:03:46 +0900

    Tree of demand

--- utils/new_script.gd
+++ utils/new_script.gd
@@ -13,53 +13,51 @@ func _ready():
 	var general = "General" if true else ""
 	var sql_stmt = sql % [intensive, general]
 	
-	var db = AssignDB.db
-	var result = db.query(sql_stmt)
-
-	# Get Courses
-	const sql_unique_courses = "SELECT DISTINCT course_code FROM studentpreferences ORDER BY course_code"
-	var has_courses = db.query(sql_unique_courses)
-	var courses = db.query_result
-	print("Courses: ", courses)
-
-
 
 	# Set up the columns
-	set_columns(3)
-	set_column_titles_visible(true)
+	set_columns(5)
+	# set_column_titles_visible(true)
 	
 	# Set column titles
 	set_column_title(0, "Course")
 	set_column_title(1, "Level")
-	set_column_title(2, "Name")
+	set_column_title(2, "Student")
+	set_column_title(3, "Choice")
+	set_column_title(4, "Total")
 	
 	# Create the root item
 	var root = create_item()
 	
-	# Sample data
-	var data = [
-		{"name": "Alice", "age": "30", "occupation": "Engineer"},
-		{"name": "Bob", "age": "25", "occupation": "Designer"},
-		{"name": "Charlie", "age": "35", "occupation": "Manager"}
-	]
-	
+	# Get Courses
+	var db = AssignDB.db
+	const sql_courses = "SELECT DISTINCT course_code FROM studentpreferences ORDER BY course_code"
+	db.query(sql_courses)
+	var courses = db.query_result
 	# Populate the tree with items
 	for course in courses:
-		var item = create_item(root)
-		item.set_text(0, course["course_code"])
-		item.set_text(1, "xxx")
-		item.set_text(2, "yyy")
+		var course_entry = root.create_child()
+		course_entry.set_text(0, course["course_code"])
+		course_entry.set_text(4, "1")
 		# Get Levels
-		var sql_unique_levels = "SELECT DISTINCT level FROM studentpreferences WHERE course_code = '%s' ORDER BY level" % course["course_code"]
-		var has_levels = db.query(sql_unique_levels)
+		var sql_levels = "SELECT DISTINCT level FROM studentpreferences WHERE course_code = '%s' ORDER BY level" % course["course_code"]
+		db.query(sql_levels)
 		var levels = db.query_result
 		for level in levels:
-			var child_item = create_item(item)
-			child_item.set_text(1,level["level"])
+			var level_entry = course_entry.create_child()
+			level_entry.set_text(0,level["level"])		
+			level_entry.set_text(1,level["level"])		
+			var sql_students = "SELECT DISTINCT student_id, firstName, lastName FROM studentpreferences WHERE course_code = '%s' AND level = '%s' ORDER BY level" % [course["course_code"], level["level"]]
+			db.query(sql_students)
+			var students = db.query_result
+			for student in students:
+				var str_entry = level_entry.create_child()
+				str_entry.set_text(2, student["firstName"] + student["lastName"])
+				str_entry.set_text(3, "Mon01")
+				str_entry.set_text(4,"88")
 	
 	# Add a child item to Alice
-	var alice_item = root.get_first_child()
-	var child_item = create_item(alice_item)
-	child_item.set_text(0, "Daughter")
-	child_item.set_text(1, "5")
-	child_item.set_text(2, "Student")
+	# var alice_item = root.get_first_child()
+	# var child_item = create_item(alice_item)
+	# child_item.set_text(0, "Daughter")
+	# child_item.set_text(1, "5")
+	# child_item.set_text(2, "Student")

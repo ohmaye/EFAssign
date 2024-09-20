fa18a1923dcfa755dd98af926d9cb7cb7cba8f60
Ohmaye <enio.ohmaye@gmail.com>
2024-09-20 09:32:35 +0900

    Will change supply_by_weekday to Tree

--- App/Assign/assign.gd
+++ App/Assign/assign.gd
@@ -1,32 +1,11 @@
 extends Supply
 
-const COLUMN_NAMES  = Constants.DEMAND_COLUMN_NAMES
-const KEY = Constants.DEMAND_KEY
-
-const sql = "SELECT * FROM demand WHERE program IN ('%s', '%s') ORDER BY firstName, lastName"
-
 # Test
 var demand_by_course = preload("res://App/assign/demand_by_course.tscn")
 var connect_scene = preload("res://App/assign/connect.tscn")
 var supply_by_weekday = preload("res://App/assign/supply_by_weekday.tscn")
 
 func render():
-	# Enable Intensive/General
-	GlobalVars.intensive_checkbox.disabled = false
-	GlobalVars.general_checkbox.disabled = false
-
-	var intensive = "Intensive" if GlobalVars.intensive_checkbox.button_pressed else ""
-	var general = "General" if GlobalVars.general_checkbox.button_pressed else ""
-	var sql_stmt = sql % [intensive, general]
-	
-	var db = AssignDB.db
-	var result = db.query(sql_stmt)
-
-	# If there are no results, return
-	if not result:
-		return
-		
-	var query_info = QueryInfo.new("demand", COLUMN_NAMES, db.query_result, KEY )
 
 	var scene = demand_by_course.instantiate()
 	%DemandTable.add_child(scene)
--- App/Assign/supply_by_weekday.gd
+++ App/Assign/supply_by_weekday.gd
@@ -1,49 +1,55 @@
 extends Supply
 
-const sql_weekdays = """
+const sql_timeslots = """
 WITH cls as (
-	SELECT courses.code as 'course', classes.title as 'class', rooms.name as 'where', timeslots.weekday || ' ' || timeslots.start_time as 'when', teachers.name as 'who' FROM classes
+	SELECT courses.code as 'course', classes.title as 'class', rooms.name as 'where', timeslots.weekday as weekday, timeslots.weekday || ' ' || timeslots.start_time as 'when', teachers.name as 'who' FROM classes
 	LEFT JOIN courses USING (course_id)
 	LEFT JOIN rooms USING (room_id)
 	LEFT JOIN timeslots USING (timeslot_id)
 	LEFT JOIN teachers USING (teacher_id)
+	ORDER BY classes.title
 	)
-SELECT DISTINCT cls.'when' FROM cls
+SELECT DISTINCT cls.'when' FROM cls JOIN weekdays ON cls.weekday = weekdays.weekday ORDER BY weekdays.sort_key
 """
 
-const sql_courses = """
-	WITH cls as (
-		SELECT courses.code as 'course', classes.title as 'class', rooms.name as 'where', timeslots.weekday || ' ' || timeslots.start_time as 'when', teachers.name as 'who' FROM classes
-		LEFT JOIN courses USING (course_id)
-		LEFT JOIN rooms USING (room_id)
-		LEFT JOIN timeslots USING (timeslot_id)
-		LEFT JOIN teachers USING (teacher_id)
-		ORDER BY classes.title
-		)
+const sql_classes = """
+WITH cls as (
+	SELECT courses.code as 'course', classes.title as 'class', rooms.name as 'where', timeslots.weekday as weekday, timeslots.weekday || ' ' || timeslots.start_time as 'when', teachers.name as 'who' FROM classes
+	LEFT JOIN courses USING (course_id)
+	LEFT JOIN rooms USING (room_id)
+	LEFT JOIN timeslots USING (timeslot_id)
+	LEFT JOIN teachers USING (teacher_id)
+	ORDER BY classes.title
+	)
 	SELECT * FROM cls
 """
 
 func _ready():
 	var db = AssignDB.db
-	var result = db.query(sql_weekdays)
-
-	var headers = ["Class"] 
-	for timeslot in db.query_result:
-		headers += [timeslot['when']]
-	print(headers)
-
-	# var query_info = QueryInfo.new("courses", COLUMN_NAMES, db.query_result, KEY )
-	
 	var label : Label 
-	for column in headers:
+
+	# Get weekdays appearing in supply
+	var result = db.query(sql_timeslots)
+	var timeslots = [] 
+	for timeslot in db.query_result:
+		timeslots += [timeslot['when']]
+	
+	# Render Headers
+	var headers = ["Class"] + timeslots
+	for column_name in headers:
 		label = %Label.duplicate()
-		label.text = column
+		label.text = column_name
 		%Header.add_child(label)
 
-	result = db.query(sql_courses)
+	# Go through classes and for each populate the intersection if it exists
+	result = db.query(sql_classes)
 	for row in db.query_result:
-		var rowContainer : HBoxContainer = HBoxContainer.new()
-		add_child(rowContainer)
+		var row_node = %Row.duplicate()
+		%Body.add_child(row_node)
 		label = %Label.duplicate()
-		label.text = row['course']
-		rowContainer.add_child(label)
+		label.text = row["class"] if row["class"] else ""
+		row_node.add_child(label)
+		for timeslot in timeslots:
+			label = %Label.duplicate()
+			label.text = "blah"
+			row_node.add_child(label)

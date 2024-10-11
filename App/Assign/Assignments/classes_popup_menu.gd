570c878007bcc4dd5c5dc27b7cea6053ad38ad48
Enio Ohmaye <enio.ohmaye@gmail.com>
2024-10-11 11:17:51 +0900

    Few changes to assignments. Not done yet.

--- App/Assign/Assignments/classes_popup_menu.gd
+++ App/Assign/Assignments/classes_popup_menu.gd
@@ -32,15 +32,33 @@ func load_and_render(item):
 func _on_assignment_selected(index):
 	printt("id_pressed -> Class ID: ", index, classes[index - 1].get('title'))
 	printt("Metadata: ", current_assignment)
-	if index == 0:
-		print("Clearing assignment")
-		# If there is an assignment, delete it
-		if current_assignment:
-			_delete_current_assignment()
 
+	# If asked to clear an empty assignment, then do nothing
+	if index == 0 and not current_assignment:
+		return
+	
+	# If the new assignment is equal to the current assignment, do nothing
+
+	# If there is an assignment, delete it
+	if current_assignment:
+		print("Clearing assignment")
+		_delete_current_assignment()
+
+	# Insert the new assignment
+	# var class_id = classes[index - 1].get('class_id')
+	# var student_id = 
+	# var sql = "INSERT INTO assignments (assignment_id, student_id, class_id) VALUES ('%s','%s', '%s')" 
+	# var sql_stmt = sql % [Utils.uuid.v4(), student_id, class_id]
+	# # print("Query: ", sql)
+	# var result = AppDB.db_run(sql_stmt)
+	# if !result:
+	# 	%StatusMsg.text = "Status: There was a problem assigning students to class."
+	# 	%StatusMsg.text += AppDB.db.error_message
+	# 	return
+
+	# Signals.emit_signal("data_changed")
 
 func _delete_current_assignment():
 	var sql = "DELETE FROM assignments WHERE assignment_id = '%s'"
 	var sql_stmt = sql % current_assignment['assignment_id']
 	AppDB.db_run(sql_stmt)
-	Signals.emit_signal("data_changed")

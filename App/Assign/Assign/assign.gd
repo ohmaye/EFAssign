1c7557691735f9bd24cb5b6d747c8009e5599f63
Enio Ohmaye <enio.ohmaye@gmail.com>
2024-10-21 10:16:15 +0900

    Few tweaks to run on Windows without errors

--- App/Assign/Assign/assign.gd
+++ App/Assign/Assign/assign.gd
@@ -9,7 +9,6 @@ func _ready():
 	var scene = demand_by_course.instantiate()
 	%DemandTable.add_child(scene)
 	scene = connect_scene.instantiate()
-	scene.call_deferred("render")
 	%AssignTable.add_child(scene)
 	var scene2 = supply_by_weekday.instantiate()
 	# scene2.call_deferred("render")
